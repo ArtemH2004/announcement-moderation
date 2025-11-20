5c418fe708999965575af43c4995c5df8756197f
ArtemH2004 <159913643+ArtemH2004@users.noreply.github.com>
2025-11-20 21:55:14 +0300

    fix. Ads list loading

--- src/common/components/loading/ads/AdsListLoading.tsx
+++ src/common/components/loading/ads/AdsListLoading.tsx
@@ -1,13 +1,21 @@
 import { AdsItemLoading } from "@/common/components/loading/ads/AdsItemLoading";
+import { TextLoading } from "@/common/components/loading/TextLoading";
 
 export const AdsListLoading = () => {
   const list = Array.from({ length: 8 });
 
   return (
-    <ul className="grid grid-cols-1 xl:grid-cols-2 gap-4">
-      {list.map((_, index) => (
-        <AdsItemLoading key={index} />
-      ))}
-    </ul>
+    <>
+      <div className="flex items-center gap-x-2 mx-4">
+        <h2 className="text-xl xs:text-2xl">Найдено объявлений:</h2>
+        <TextLoading sizeClassName="w-20 h-7.5" />
+      </div>
+
+      <ul className="grid grid-cols-1 xl:grid-cols-2 gap-4">
+        {list.map((_, index) => (
+          <AdsItemLoading key={index} />
+        ))}
+      </ul>
+    </>
   );
 };
